61034f1a56b0a85bdff627a357a17470f8a639ec
qjalti <stig.guriev@gmail.com>
2025-12-02 18:11:13 +0300

    refactoring, add icons, add context menu,

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-const {app, BrowserWindow, Tray, Menu, nativeImage} = require("electron");
+const { app, BrowserWindow, Tray, Menu, nativeImage } = require("electron");
 
 const PATH = require("path");
 
-const PATH_TO_TRAY_ICON = "./assets/christmas_lights.ico";
+const PATH_TO_ICON = "./assets/icon.ico";
+const PATH_TO_TRAY_ICON = "./assets/christmas-lights-32.ico";
 
-let tray = null
+let tray = null;
 
 const createWindow = () => {
   const WIN = new BrowserWindow({
-    width: 5120,
+    width: 2560,
     height: 100,
     x: 0,
     y: 0,
@@ -20,6 +21,7 @@ const createWindow = () => {
     movable: false,
     focusable: false,
     minimizable: false,
+    icon: nativeImage.createFromPath(PATH_TO_ICON),
   });
 
   WIN.setIgnoreMouseEvents(true);
@@ -28,7 +30,7 @@ const createWindow = () => {
 
 const createWindow2 = () => {
   const WIN2 = new BrowserWindow({
-    width: 5120,
+    width: 2560,
     height: 100,
     x: -2560,
     y: 0,
@@ -40,6 +42,7 @@ const createWindow2 = () => {
     movable: false,
     focusable: false,
     minimizable: false,
+    icon: nativeImage.createFromPath(PATH_TO_ICON),
   });
 
   WIN2.setIgnoreMouseEvents(true);
@@ -47,15 +50,17 @@ const createWindow2 = () => {
 };
 
 app.whenReady().then(() => {
-  tray = new Tray(PATH.join(__dirname, PATH_TO_TRAY_ICON))
+  tray = new Tray(PATH.join(__dirname, PATH_TO_TRAY_ICON));
 
-  const CONTEXT_MENU = Menu.buildFromTemplate([{
-    label: "Exit",
-    type: "normal",
-    click: () => {
-      app.quit()
-    }
-  }])
+  const CONTEXT_MENU = Menu.buildFromTemplate([
+    {
+      label: "Exit",
+      type: "normal",
+      click: () => {
+        app.quit();
+      },
+    },
+  ]);
 
   tray.setToolTip("lightRope");
   tray.setContextMenu(CONTEXT_MENU);
